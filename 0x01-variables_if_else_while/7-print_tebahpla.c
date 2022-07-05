5d04e78d8edae008e1f990f7986f1223caae48bd
Mon-Oko <okotexico19@gmail.com>
2022-07-04 21:27:11 -0700

    Recent Corrections

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
- *
  *  * main - prints the alphabet in reverse.
- *
- *  (*
- *
  *   * Return: 0 on success
- *
- *    */
+*/
 int main(void)
+
 {
 char c = 'z';
 while (c >= 'a')
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
- *
  *  * main - prints all possible combinations of single digit numbers.
- *
- *  (*
- *
  *   * Return: 0 on success
- *
- *    */
+*/
 int main(void)
+
 {
 int i = '0';
 while (i <= '9')
