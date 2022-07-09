bdb570ca582480c8c2b52e34aa43677277c3d427
Mon-Oko <okotexico19@gmail.com>
2022-07-09 02:22:45 -0700

    final commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *  Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int j = 3, k = 612852475143;
+
+	for (; j < 12057; j += 2)
+	{
+		while (k % j == 0 && k != i)
+			k /= i;
+	}
+	printf("%lu\n", k);
+	return (0);
+}
